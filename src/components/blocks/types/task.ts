581fe44abaae10090aedc5cde0c9f5077058c3ec
JanithPerera <janithper9@gmail.com>
2025-04-06 17:30:31 +0530

    fixes

--- src/components/blocks/types/task.ts
+++ src/components/blocks/types/task.ts
@@ -1,9 +1,16 @@
 // types/task.ts
+export interface AssignedPerson {
+  id: string;
+  name: string;
+  avatar: string;
+}
+
+
 export type Task = {
     id: string
     title: string
     description: string
-    avatar: string
+    assignedPeople: AssignedPerson[];
     status: 'todo' | 'in-progress' | 'done'
     colorMode:'success' | 'danger' | 'warning' | 'info'
   }
