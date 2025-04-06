3403aeebb86e084b7856e84c2a02f93a8f9192eb
JanithPerera <janithper9@gmail.com>
2025-04-06 18:23:04 +0530

    mode button fix

--- src/components/blocks/page-header.tsx
+++ src/components/blocks/page-header.tsx
@@ -6,7 +6,7 @@ import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbP
 import { Separator } from "../ui/separator";
 import { Button } from "../ui/button";
 import { Sun, Moon, Bell } from "lucide-react";
-import { useThemeStore } from "@/store/useThemeStore"; // Import the Zustand store
+import { useTheme } from "next-themes";
 
 interface PageHeaderProps {
   firstLinkName: string;
@@ -14,18 +14,19 @@ interface PageHeaderProps {
 }
 
 const PageHeader: React.FC<PageHeaderProps> = ({ firstLinkName, secondLinkName }) => {
-  const { isDarkMode, toggleTheme, initializeTheme } = useThemeStore();
-
-  useEffect(() => {
-    initializeTheme();
-  }, [initializeTheme]);
+  const { theme, setTheme } = useTheme();
 
   return (
     <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
       <div className="flex items-center gap-2 px-4">
         <SidebarTrigger className="-ml-1" />
-        <Button variant="ghost" size="icon" className="size-7" onClick={toggleTheme}>
-          {isDarkMode ? <Moon /> : <Sun />}
+        <Button 
+          variant="ghost" 
+          size="icon" 
+          className="size-7" 
+          onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+        >
+          {theme === "dark" ? <Moon /> : <Sun />}
         </Button>
         <Button variant="ghost" size="icon" className="size-7">
           <Bell />
