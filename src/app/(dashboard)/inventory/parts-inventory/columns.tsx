6fe19871ab9410b2d5a33cb0de1790f8833a8262
JanithPerera <janithper9@gmail.com>
2025-05-16 10:40:47 +0530

    temp fix 4 part-inventory page

--- src/app/(dashboard)/inventory/parts-inventory/columns.tsx
+++ src/app/(dashboard)/inventory/parts-inventory/columns.tsx
@@ -119,31 +119,31 @@ export const columns: ColumnDef<PartsInventory>[] = [
       )
     },
   },
-  {
-    id: "actions",
-    cell: ({ row }) => {
-      const partsinventory = row.original
+  // {
+  //   id: "actions",
+  //   cell: ({ row }) => {
+  //     const partsinventory = row.original
  
-      return (
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="ghost" className="h-8 w-8 p-0 cursor-pointer">
-              <span className="sr-only">Open menu</span>
-              <MoreHorizontal className="h-4 w-4" />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuLabel>Actions</DropdownMenuLabel>
-            <DropdownMenuItem
-              onClick={() => navigator.clipboard.writeText(partsinventory._id)}
-            >
-              Edit
-            </DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>Delete</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      )
-    },
-  },
+  //     return (
+  //       <DropdownMenu>
+  //         <DropdownMenuTrigger asChild>
+  //           <Button variant="ghost" className="h-8 w-8 p-0 cursor-pointer">
+  //             <span className="sr-only">Open menu</span>
+  //             <MoreHorizontal className="h-4 w-4" />
+  //           </Button>
+  //         </DropdownMenuTrigger>
+  //         <DropdownMenuContent align="end">
+  //           <DropdownMenuLabel>Actions</DropdownMenuLabel>
+  //           <DropdownMenuItem
+  //             onClick={() => navigator.clipboard.writeText(partsinventory._id)}
+  //           >
+  //             Edit
+  //           </DropdownMenuItem>
+  //           <DropdownMenuSeparator />
+  //           <DropdownMenuItem>Delete</DropdownMenuItem>
+  //         </DropdownMenuContent>
+  //       </DropdownMenu>
+  //     )
+  //   },
+  // },
 ]
