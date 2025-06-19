3fc99fc932f6f2ae4fe533c83e80dbdf5bf237d9
JanithPerera <janithper9@gmail.com>
2025-06-20 00:34:21 +0530

    low stock bar char added

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -7,6 +7,7 @@ import { PartsStockChart } from "./dashboard-components/parts-stock-chart";
 import { ChartPieSimple } from "./dashboard-components/chart-pie-simple";
 import { KanbanTasksChart } from "./dashboard-components/kanban-task-chart";
 import { ChartBarMultiple } from "./dashboard-components/chart-bar-multiple";
+import { LowStockPartsChart } from "./dashboard-components/low-stock-parts-chart";
 
 export default function Page() {
   return (
@@ -24,7 +25,7 @@ export default function Page() {
             <KanbanTasksChart />
           </div>
           <div className="bg-muted/50 rounded-xl h-full">
-            <ChartBarMultiple />
+            <LowStockPartsChart />
           </div>
         </div>
         <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
